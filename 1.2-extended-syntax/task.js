931219e925935052955a6e39d25713dbb7c3a84e
Richard Winter <a.blake.mail@gmail.com>
2020-06-10 12:04:01 +0300

    1.2 complete

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,48 @@
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
+    'use strict';
+    let dis;
+    let array = [];
+
+    dis = Math.pow(b, 2) - 4 * a * c;
+
+    if (dis === 0) {
+        array.push(-b / 2 * a); 
+     } else if (dis > 0) {
+        array.push(-b / ( 2 * a ) + Math.sqrt(dis) / (2 * a));
+        array.push(-b / ( 2 * a ) - Math.sqrt(dis) / (2 * a));
+     }
+     return array;
     // return x;
 }
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-    // return averageMark;
+    let averageMark;
+
+    if (marks.length === 0) return 0;
+    if (marks.length > 5) {
+        console.log('Оценок больше 5ти');
+        marks.splice(5, marks.length - 4)
+      }
+      let sum = 0;
+      for (let i = 0; i < marks.length; i++) {
+          sum = sum + marks[i];
+      }
+      averageMark = sum / marks.length;
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    // return result;
+    let date = new Date();
+    let result = '';
+
+    if ((date - dateOfBirthday) >= 18) {
+        result += `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        result += `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+
+    return result;
 }
