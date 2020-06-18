18ba946e562d4c7a29a1c9c5954eb18ec68ca177
Richard Winter <a.blake.mail@gmail.com>
2020-06-18 16:04:59 +0300

    2.2 complete v2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -2,25 +2,25 @@
 String.prototype.isPalindrome = function() {
 	str = JSON.stringify(this);
 	str = str.toLowerCase().replace(/[^а-яa-z1-9]/gi, '');
-    const lastIndex = str.length - 1;
-    for (let i = 0; i < str.length / 2; i++) {
-        if (str[i] !== str[lastIndex - i]) {
-            return false;
-        }
-    }
-    return true;
+	const lastIndex = str.length - 1;
+	for (let i = 0; i < str.length / 2; i++) {
+		if (str[i] !== str[lastIndex - i]) {
+			return false;
+		}
+	}
+	return true;
 };
 
 function getAverageMark(marks) {
-		// код для задачи №2 писать здесь
-		// почему так просто? Это уже делали
-		if (marks.length === 0) return 0;
-		let sum = 0;
-		for (let i = 0; i < marks.length; i++) {
-				sum = sum + marks[i];
-		}
-		average = sum / marks.length;
-		return roundedAverage = Math.round(average);
+	// код для задачи №2 писать здесь
+	// почему так просто? Это уже делали
+	if (marks.length === 0) return 0;
+	let sum = 0;
+	for (let i = 0; i < marks.length; i++) {
+		sum = sum + marks[i];
+	}
+	average = sum / marks.length;
+	return roundedAverage = Math.round(average);
 }
 
 function checkBirthday(birthday) {
